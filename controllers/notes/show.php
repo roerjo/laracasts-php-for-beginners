3cecfbf295107fdba98f17f117f32b17265247c0
Eric Johnson <eric.johnson@aware3.com>
2023-02-20 08:11:03 -0500

    Lesson32

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,15 +5,30 @@ use Core\Database;
 $config = require(base_path('config.php'));
 $db = new Database($config['database']);
 
-$note = $db->query('select * from notes where id = :id', [
-    'id' => $_GET['id'],
-])->findOrFail();
-
 $currentUserId = 1;
 
-authorize($note['user_id'] === $currentUserId);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_POST['id'],
+    ])->findOrFail();
 
-require view('notes/show.view.php', [
-    'heading' => 'Note',
-    'notes' => $note,
-]);
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $_POST['id'],
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id'],
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    require view('notes/show.view.php', [
+        'heading' => 'Note',
+        'notes' => $note,
+    ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -8,6 +8,11 @@
             <a href="/notes" class="text-blue-500 hover:underline">Go back...</a>
         </p>
         <p><?= htmlspecialchars($note['body']) ?></p>
+
+        <form class="mt-5" method="POST">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>"/>
+            <button class="text-sm text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 
