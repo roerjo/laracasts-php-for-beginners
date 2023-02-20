d08314fa2c0a793f8989797bdbc9f569d20f359d
Eric Johnson <eric.johnson@aware3.com>
2023-02-20 06:43:09 -0500

    Lesson5

--- index.php
+++ index.php
@@ -3,14 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>
-        <?php
-            $greeting = 'Hello';
+    <?php
+        $name = 'Dark Matter';
+        $read = true;
 
-            echo "{$greeting} Everybody!";
-        ?>
+        if ($read) {
+            $message = "You have read {$name}";
+        } else {
+            $message = "You have NOT read {$name}";
+        }
+    ?>
+
+    <h1>
+        <?= $message ?>
     </h1>
 </body>
 </html>
