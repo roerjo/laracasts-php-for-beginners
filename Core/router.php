d00e7b2b9b62d7403c79071a1a96453d0d22d69d
Eric Johnson <eric.johnson@aware3.com>
2023-02-20 07:41:43 -0500

    Lesson31

--- Core/router.php
+++ Core/router.php
@@ -1,18 +1,18 @@
 <?php
 
-$routes = require ('routes.php');
+$routes = require base_path('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 function abort($status = 404) {
     http_response_code($status);
 
-    include "views/{$status}.php";
+    include base_path("views/{$status}.php");
 }
 
 function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     }
 
     abort(404);
