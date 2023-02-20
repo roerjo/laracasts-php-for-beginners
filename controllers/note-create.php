69d1e28d34c318b8b84d9f3e0ff4596a996940a4
Eric Johnson <eric.johnson@aware3.com>
2023-02-20 07:16:58 -0500

    Lesson27

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,23 @@ $db = new Database($config['database']);
 $heading = "Create A Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('insert into notes (user_id, body) values (:user_id, :body)', [
-        'body' => $_POST['body'],
-        'user_id' => 1,
-    ]);
+    $errors = [];
+
+
+    if (empty($_POST['body'])) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > 45) {
+        $errors['body'] = 'Body cannot be longer than 45 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query('insert into notes (user_id, body) values (:user_id, :body)', [
+            'body' => $_POST['body'],
+            'user_id' => 1,
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
