eb82e9c85614d5d0a6540f2f87fc012e69f7715c
Eric Johnson <eric.johnson@aware3.com>
2023-02-20 07:11:43 -0500

    Lesson23

--- controllers/note.php
+++ controllers/note.php
@@ -5,6 +5,18 @@ $db = new Database($config['database']);
 
 $heading = "Note";
 
-$note = $db->query('select * from notes where user_id = 1 and id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id'],
+])->fetch();
+
+if (! $note) {
+    abort(Response::NOT_FOUND);
+}
+
+$currentUserId = 1;
+
+if (! $note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require 'views/note.view.php';
